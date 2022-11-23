28922e2b663bb207deabd05e54f0db7d2ad7ecd6
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-23 20:46:49 -0300

    jsx estructure app

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import styles from './styles/GlobalStyles';
 
 export default function App() {
   return (
-    <View style={styles.app}>
-      <MapView
-        style={styles.containerMap}
-      />
-    </View>
+    <>
+
+      <StatusBar />
+      <View style={styles.app}>
+        <View>
+          <Text>GooGleMapTemplet</Text>
+          <View tyle={styles.containerMap}>
+            <MapView
+              style={styles.map}
+            />
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
--- styles/GlobalStyles.jsx
+++ styles/GlobalStyles.jsx
@@ -6,11 +6,13 @@ const styles = StyleSheet.create({
     app:{
         flex: 1,
         justifyContent:'center',
-        
     },
     containerMap:{
+        marginHorizontal:10,
+    },
+    map:{
         width:'100%',
         height:'100%',
-    },
+    }
 });
 export default styles

424d2fe937564d53219b8e38507ffad5d8af31e3
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-23 20:33:17 -0300

    Import Global Styles

---
+++ styles/GlobalStyles.jsx
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native'
+
+
+
+const styles = StyleSheet.create({
+    app:{
+        flex: 1,
+        justifyContent:'center',
+        
+    },
+    container:{
+        marginHorizontal:'2.5%'
+    },
+});
+export default styles
